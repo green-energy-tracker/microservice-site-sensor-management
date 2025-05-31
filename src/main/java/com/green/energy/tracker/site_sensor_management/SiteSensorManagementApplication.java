30e1283f9dfc01357b1f18368dbbf09919b7628e
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-05-31 13:52:00 +0200

    add sensor and site repo

--- src/main/java/com/green/energy/tracker/site_sensor_management/SiteSensorManagementApplication.java
+++ src/main/java/com/green/energy/tracker/site_sensor_management/SiteSensorManagementApplication.java
@@ -2,7 +2,9 @@ package com.green.energy.tracker.site_sensor_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class SiteSensorManagementApplication {
 
