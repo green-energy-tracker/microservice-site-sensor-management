aabfc8d01a17f98286e7f6453fdc63b73fac2cd4
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-06-03 00:43:56 +0200

    add sensor and site repo

--- src/main/java/com/green/energy/tracker/site_sensor_management/client/UserManagementClientFallbackFactory.java
+++ src/main/java/com/green/energy/tracker/site_sensor_management/client/UserManagementClientFallbackFactory.java
@@ -2,8 +2,10 @@ package com.green.energy.tracker.site_sensor_management.client;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FallbackFactory;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class UserManagementClientFallbackFactory implements FallbackFactory<UserManagementClient> {
     @Override
     public UserManagementClient create(Throwable cause) {
