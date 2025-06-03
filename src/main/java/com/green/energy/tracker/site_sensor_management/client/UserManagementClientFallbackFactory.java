1c75ff8bab65701f286df390225ada430ae9a896
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-06-03 10:50:04 +0200

    add sensor and site repo

--- src/main/java/com/green/energy/tracker/site_sensor_management/client/UserManagementClientFallbackFactory.java
+++ src/main/java/com/green/energy/tracker/site_sensor_management/client/UserManagementClientFallbackFactory.java
@@ -1,5 +1,6 @@
 package com.green.energy.tracker.site_sensor_management.client;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.stereotype.Component;
@@ -11,5 +12,10 @@ public class UserManagementClientFallbackFactory implements FallbackFactory<User
     public UserManagementClient create(Throwable cause) {
         log.error("Fallback triggered due to: {}", cause.toString());
         return username -> {throw new UserManagementUnavailableException("UserManagement service not reachable", cause);};
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("UserManagementClientFallbackFactory initialized");
     }
 }
